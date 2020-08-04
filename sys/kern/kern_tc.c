9cc9911c4da58765634d768271c0a40bcffa0dc4
Yuichiro NAITO <naito.yuichiro@gmail.com>
2020-08-04 13:59:58 +0900

    re-apply tickless patch.
      commit 4b4793304325522e159494e65cc0b3cd5a4dfa75
      Author: cheloha <cheloha@openbsd.org>
      Date:   Tue Nov 26 15:27:08 2019 +0000
    
          timeout(9): switch to tickless backend

--- sys/kern/kern_tc.c
+++ sys/kern/kern_tc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_tc.c,v 1.55 2019/12/12 19:30:21 cheloha Exp $ */
+/*	$OpenBSD: kern_tc.c,v 1.51 2019/11/26 15:27:08 cheloha Exp $ */
 
 /*
  * Copyright (c) 2000 Poul-Henning Kamp <phk@FreeBSD.org>
@@ -460,7 +460,7 @@ tc_setclock(const struct timespec *ts)
 	if (adj_ticks > 0) {
 		if (adj_ticks > INT_MAX)
 			adj_ticks = INT_MAX;
-		timeout_adjust_ticks(adj_ticks);
+		ticks += adj_ticks;
 	}
 #endif
 }
