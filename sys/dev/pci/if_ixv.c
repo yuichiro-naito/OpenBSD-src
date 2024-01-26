0657c9aae0b8719b3a4340886cffa2e07d9b414a
Yuichiro NAITO <naito.yuichiro@gmail.com>
2024-01-26 11:49:10 +0900

    Rename 'ifq_set_maxlen' to 'ifq_init_maxlen'.

--- sys/dev/pci/if_ixv.c
+++ sys/dev/pci/if_ixv.c
@@ -775,7 +775,7 @@ ixv_setup_interface(struct device *dev, struct ix_softc *sc)
 	ifp->if_watchdog = ixv_watchdog;
 	ifp->if_hardmtu = IXGBE_MAX_FRAME_SIZE -
 	    ETHER_HDR_LEN - ETHER_CRC_LEN;
-	ifq_set_maxlen(&ifp->if_snd, sc->num_tx_desc - 1);
+	ifq_init_maxlen(&ifp->if_snd, sc->num_tx_desc - 1);
 
 	ifp->if_capabilities = IFCAP_VLAN_MTU;
 
