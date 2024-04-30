2be368cbe149df6a4597f7b0d5a18a07c2ebfddb
Yuichiro NAITO <naito.yuichiro@gmail.com>
2024-04-30 23:48:08 +0900

    OpenBSD 7.5 Release

--- regress/sys/kern/unfdpass/unfdpass.c
+++ regress/sys/kern/unfdpass/unfdpass.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unfdpass.c,v 1.23 2023/03/08 04:43:06 guenther Exp $	*/
+/*	$OpenBSD: unfdpass.c,v 1.24 2023/03/08 04:43:06 guenther Exp $	*/
 /*	$NetBSD: unfdpass.c,v 1.3 1998/06/24 23:51:30 thorpej Exp $	*/
 
 /*-
